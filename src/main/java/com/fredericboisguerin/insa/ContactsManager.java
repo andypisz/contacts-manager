b0cb3d86a9ad240ba82ea429eb431ffe1b2e30c3
piszyna <a.piszyna@gmail.com>
2017-11-10 10:42:47 +0100

    Le test  list one contact without email nor phone number fonctionne.

--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -2,12 +2,19 @@ package com.fredericboisguerin.insa;
 
 public class ContactsManager {
 
-    public void addContact(String name, String email, String phoneNumber) {
+    private String name;
+    //private String email;
+    //private String phoneNumber;
 
+
+    public void addContact(String name, String email, String phoneNumber) {
+        this.name = name;
+        //this.email = email;
+        //this.phoneNumber = phoneNumber;
     }
 
     public void printAllContacts() {
-
+        System.out.println(this.name);
     }
 
     public void searchContactByName(String name) {
