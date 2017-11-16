d91fb5cbcfee7aff42821a31e56f4fec1d03c8c7
piszyna <a.piszyna@gmail.com>
2017-11-16 11:48:37 +0100

    Implémentation de base fonctionnelle de l'interface utilisateur

---
+++ src/main/java/com/fredericboisguerin/insa/InterfaceUtilisateur.java
@@ -0,0 +1,98 @@
+package com.fredericboisguerin.insa;
+
+import java.util.Scanner;
+
+public class InterfaceUtilisateur {
+
+    private ContactsManager cm;
+
+    public InterfaceUtilisateur(ContactsManager cm) throws InvalidEmailException, InvalidContactNameException {
+        this.cm = cm;
+        int choix;
+
+        System.out.println("***** BIENVENU DANS L'ANNUAIRE DE CONTACTS *****");
+        System.out.println();
+
+        do{
+            printMenu();
+            choix = getSelection();
+            actionApresSelectionUtilisateur(choix);
+        } while (choix != 0);
+
+    }
+
+    public void printMenu(){
+        System.out.println("Tapez le numéro de votre choix :");
+        System.out.println();
+        System.out.println("1 - Ajouter un contact à l'annuaire");
+        System.out.println("2 - Afficher tous les contacts de l'annuaire");
+        System.out.println("3 - Chercher un contact à l'aide de son nom");
+        System.out.println("0 - Quitter");
+        System.out.println();
+        System.out.println("Votre choix : ");
+    }
+
+    public int getSelection(){
+        Scanner sc = new Scanner(System.in);
+        int choix = sc.nextInt();
+        System.out.println();
+        return choix;
+    }
+
+    public void actionAjouterContact() throws InvalidEmailException, InvalidContactNameException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vous avez choisi d'ajouter un contact à l'annuaire.");
+        System.out.println();
+        System.out.println("Veuillez entrer son nom : ");
+        String nom = sc.nextLine();
+        System.out.println("Veuillez entrer son email : ");
+        String email = sc.nextLine();
+        System.out.println("Veuillez entre son numéro de téléphone : ");
+        String phoneNumber = sc.nextLine();
+        //System.out.println("nom : "+nom+", email : "+email+", phone number : "+phoneNumber);
+        cm.addContact(nom,email,phoneNumber);
+
+    }
+
+    public void actionAfficherContacts(){
+        System.out.println("Vous avez choisi d'afficher tous les contacts de l'annuaire.");
+        System.out.println();
+        this.cm.printAllContacts();
+    }
+
+    public void actionChercherContact(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vous avez choisi de chercher un contact dans l'annuaire à l'aide de son nom.");
+        System.out.println();
+        System.out.println("Veuillez entrer son nom : ");
+        String nom = sc.nextLine();
+        cm.searchContactByName(nom);
+    }
+
+    public void actionQuitter(){
+        System.out.println("Merci d'avoir utilisé notre annuaire de contacts.");
+        System.out.println();
+        System.out.println("***** A BIENTOT *****");
+    }
+
+    public void actionApresSelectionUtilisateur(int choixUtilisateur) throws InvalidEmailException, InvalidContactNameException {
+        switch (choixUtilisateur) {
+            case 0 :
+                actionQuitter();
+                break;
+            case 1 :
+                actionAjouterContact();
+                break;
+            case 2 :
+                actionAfficherContacts();
+                break;
+            case 3 :
+                actionChercherContact();
+                break;
+            default:
+                //erreur
+                break;
+
+        }
+    }
+}
---
+++ src/main/java/com/fredericboisguerin/insa/Main.java
@@ -0,0 +1,9 @@
+package com.fredericboisguerin.insa;
+
+public class Main {
+
+    public static void main(String[] args) throws InvalidEmailException, InvalidContactNameException {
+        ContactsManager monContactsManager = new ContactsManager();
+        InterfaceUtilisateur monInterface = new InterfaceUtilisateur(monContactsManager);
+    }
+}
